48cd0bb0f4da3adcba7d87dbdad987257df3b020
Atherizz <saveroathallah86@gmail.com>
2026-06-25 18:33:34 +0700

    feat: add agym use command and swap logic

---
+++ internal/profile/swap.go
@@ -0,0 +1,87 @@
+package profile
+
+import (
+
+    "io"
+
+    "os"
+
+    "path/filepath"
+
+)
+
+func StashCurrentProfile(p *PathResolver, name string) error {
+
+    for _, f := range p.IsolatedFiles() {
+
+        if err := copyIfExists(filepath.Join(p.GeminiRoot(), f), filepath.Join(p.ProfileDir(name), f)); err != nil { return err }
+
+    }
+
+    for _, d := range p.IsolatedDirs() {
+
+        if err := moveIfExists(filepath.Join(p.GeminiRoot(), d), filepath.Join(p.ProfileDir(name), d)); err != nil { return err }
+
+    }
+
+    return nil
+
+}
+
+func SwapToProfile(p *PathResolver, name string) error {
+
+    for _, f := range p.IsolatedFiles() {
+
+        if err := copyIfExists(filepath.Join(p.ProfileDir(name), f), filepath.Join(p.GeminiRoot(), f)); err != nil { return err }
+
+    }
+
+    for _, d := range p.IsolatedDirs() {
+
+        if err := moveIfExists(filepath.Join(p.ProfileDir(name), d), filepath.Join(p.GeminiRoot(), d)); err != nil { return err }
+
+    }
+
+    return nil
+
+}
+
+func copyIfExists(src, dst string) error {
+
+    srcFile, err := os.Open(src)
+
+    if err != nil {
+
+        if os.IsNotExist(err) { return nil }
+
+        return err
+
+    }
+
+    defer srcFile.Close()
+
+    os.MkdirAll(filepath.Dir(dst), 0755)
+
+    dstFile, err := os.Create(dst)
+
+    if err != nil { return err }
+
+    defer dstFile.Close()
+
+    _, err = io.Copy(dstFile, srcFile)
+
+    return err
+
+}
+
+func moveIfExists(src, dst string) error {
+
+    if _, err := os.Stat(src); os.IsNotExist(err) { return nil }
+
+    os.RemoveAll(dst)
+
+    os.MkdirAll(filepath.Dir(dst), 0755)
+
+    return os.Rename(src, dst)
+
+}
